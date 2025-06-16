d0453028ba21dbeca3901107dda8b7a9c6a374e1
ChoiWanyoung <wy.choi.pe@gmail.com>
2025-06-16 17:16:03 +0900

    initial commit

--- src/app/(custom)/page.tsx
+++ src/app/(custom)/page.tsx
@@ -1,25 +1,11 @@
-import PageBreadcrumb from "@/components/common/PageBreadCrumb";
 import DefaultInputs from "@/components/form/form-elements/DefaultInputs";
-import InputGroup from "@/components/form/form-elements/InputGroup";
-import { Metadata } from "next";
-import React from "react";
-
-export const metadata: Metadata = {
-  title: "Next.js Form Elements | TailAdmin - Next.js Dashboard Template",
-  description:
-    "This is Next.js Form Elements page for TailAdmin - Next.js Tailwind CSS Admin Dashboard Template",
-};
 
 export default function FormElements() {
   return (
     <div>
-      <PageBreadcrumb pageTitle="견적 계산기" />
       <div className="grid grid-cols-1 gap-6 xl:grid-cols-2">
         <div className="space-y-6">
           <DefaultInputs />
-        </div>
-        <div className="space-y-6">
-          <InputGroup />
         </div>
       </div>
     </div>
